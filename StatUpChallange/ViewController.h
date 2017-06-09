c7a09f74c5e8010d7748db50d09959c6a5d6cc83
Shah Yasin <shahyasin@Shahs-MacBook-Pro.local>
2017-06-09 12:44:03 +0600

    Initial Commit

---
+++ StatUpChallange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StatUpChallange
+//
+//  Created by Shah Yasin on 6/9/17.
+//  Copyright © 2017 Shah Yasin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StatUpChallange/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StatUpChallange
+//
+//  Created by Shah Yasin on 6/9/17.
+//  Copyright © 2017 Shah Yasin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
